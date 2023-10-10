493e9899e6dc977c0260d4bc3860762c09c949ec
jmcjm <jkmendyka@gmail.com>
2023-10-10 21:10:13 +0100

    v0.5

--- projekt_1/Program.cs
+++ projekt_1/Program.cs
@@ -14,7 +14,7 @@ namespace projekt_1
             Console.WriteLine("2) Statystyki dla gracza");
             Console.WriteLine("3) Wyjście");
             Console.Write("Co chesz zrobić?: ");
-            choose = inputLibrary.Int.restricted_int_input(1,3);
+            choose = inputLibrary.Int.restricted_int_input(1, 3);
             switch (choose)
             {
                 case 1:
@@ -34,29 +34,40 @@ namespace projekt_1
         }
         static void game()
         {
-            char[,] cords = new char[3, 3];
-            int x_or_o=0;
+            char[] cords = new char[9];
+            int x_or_o = 0;
             for (int i = 0; i < cords.GetLength(0); i++)
             {
-                for (int j = 0; j < cords.GetLength(1); j++)
-                {
-                    cords[i, j] = ' ';
-                }
+                cords[i] = ' ';
             }
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Aby przerwać grę wpisz 0 jako miejsce pionowe (postęp zostanie utracony)");
-            while (true) 
+            while (true)
             {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine
-                    (" {0} | {1} | {2} \n" +
-                     "-----------\n" +
-                     " {3} | {4} | {5} \n" +
-                     "-----------\n" +
-                     " {6} | {7} | {8} ",
-                     cords[0, 0], cords[0, 1], cords[0, 2],
-                     cords[1, 0], cords[1, 1], cords[1, 2],
-                     cords[2, 0], cords[2, 1], cords[2, 2]);
-                Console.ResetColor();
+                if (x_or_o == 0)
+                {
+                    Console.WriteLine
+                        (" 1 | 2 | 3 \n" +
+                         "-----------\n" +
+                         " 4 | 5 | 6 \n" +
+                         "-----------\n" +
+                         " 7 | 8 | 9 ");
+                }
+                else
+                {
+                    Console.WriteLine
+                        (" {0} | {1} | {2} \n" +
+                         "-----------\n" +
+                         " {3} | {4} | {5} \n" +
+                         "-----------\n" +
+                         " {6} | {7} | {8} ",
+                         cords[0], cords[1], cords[2],
+                         cords[3], cords[4], cords[5],
+                         cords[6], cords[7], cords[8]);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
                 if (win_checking(cords) == 1)
                 {
                     if (x_or_o % 2 == 0)
@@ -73,76 +84,74 @@ namespace projekt_1
                     Console.ReadKey();
                     break;
                 }
-                else if (x_or_o % 2 == 0) 
+                else if (x_or_o % 2 == 0)
                     next_move(cords, 'X');
-                else 
+                else
                     next_move(cords, 'O');
                 x_or_o++;
                 Console.Clear();
             }
         }
-        static char[,] next_move(char[,] cords, char x_o)
+        static char[] next_move(char[] cords, char x_o)
         {
             int x, y;
-            while(true) 
+            while (true)
             {
-                Console.Write("Wybierz pionowe miejsce {0}: ", x_o);
-                x = inputLibrary.Int.restricted_int_input(0, 3);
+                Console.Write("Wybierz miejsce {0}: ", x_o);
+                x = inputLibrary.Int.restricted_int_input(0, 9);
                 if (x == 0)
                     Main();
-                Console.Write("Wybierz poziome miejsce {0}: ", x_o);
-                y = inputLibrary.Int.restricted_int_input(1, 3);
-                if (cords[x - 1, y - 1] == ' ')
+                if (cords[x - 1] == ' ')
                 {
-                    cords[x - 1, y - 1] = x_o;
+                    cords[x - 1] = x_o;
                     break;
                 }
                 Console.WriteLine("Miejsce zajęte!");
             }
             return cords;
         }
-        private static int win_checking(char[,] cords)
+        private static int win_checking(char[] cords)
         {
             #region Horzontal Winning Condtion
             //Winning Condition For First Row
-            if (cords[0, 0] != ' ' && cords[0, 0] == cords[0, 1] && cords[0, 1] == cords[0, 2])
+            if (cords[0] != ' ' && cords[0] == cords[1] && cords[1] == cords[2])
             {
                 return 1;
             }
             //Winning Condition For Second Row
-            else if (cords[1, 0] != ' ' &&  cords[1, 0] == cords[1, 1] && cords[1, 1] == cords[1, 2])
+            else if (cords[3] != ' ' && cords[3] == cords[4] && cords[4] == cords[5])
             {
                 return 1;
             }
             //Winning Condition For Third Row
-            else if (cords[2, 0] != ' ' && cords[2, 0] == cords[2, 1] && cords[2, 1] == cords[2, 2])
+            else if (cords[6] != ' ' && cords[6] == cords[7] && cords[7] == cords[8])
             {
                 return 1;
             }
             #endregion
             #region vertical Winning Condtion
             //Winning Condition For First Column
-            else if (cords[0, 0] != ' ' && cords[0, 0] == cords[1, 0] && cords[1, 0] == cords[2, 0])
+            else if (cords[0] != ' ' && cords[0] == cords[3] && cords[3] == cords[6])
             {
                 return 1;
             }
             //Winning Condition For Second Column
-            else if (cords[0, 1] != ' ' && cords[0, 1] == cords[1, 1] && cords[1, 1] == cords[2, 1])
+            else if (cords[1] != ' ' && cords[1] == cords[4] && cords[4] == cords[7])
             {
                 return 1;
             }
             //Winning Condition For Third Column
-            else if (cords[0, 2] != ' ' && cords[0, 2] == cords[1, 2] && cords[1, 2] == cords[2, 2])
+            else if (cords[2] != ' ' && cords[2] == cords[5] && cords[5] == cords[8])
             {
                 return 1;
             }
             #endregion
             #region Diagonal Winning Condition
-            else if (cords[0, 0] != ' ' && cords[0, 0] == cords[1, 1] && cords[1, 1] == cords[2, 2])
+            else if (cords[0] != ' ' && cords[0] == cords[4] && cords[4] == cords[8])
             {
                 return 1;
             }
-            else if (cords[2, 0] != ' ' &&cords[2, 0] == cords[1, 1] && cords[1, 1] == cords[0, 2])
+            else if (cords[6] != ' ' && cords[6] == cords[4] && cords[4] == cords[2])
             {
                 return 1;
             }
