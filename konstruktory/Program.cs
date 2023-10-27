f0157980e4250c05ad790133eabedda7571de997
jmcjm <jkmendyka@gmail.com>
2023-10-27 14:55:33 +0100

    konstruktory

---
+++ konstruktory/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace konstruktory
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public Engine Engine { get; set; }
+            public Car(string brand, string model, double capacity, double fuelAmaount)
+            {
+                this.Brand = brand;
+                this.Model = model;
+                this.Engine.Capacity = capacity;
+                this.Engine.FuelAmaount = fuelAmaount;
+            }
+            public Car(string brand, string model, double capacity, double fuelAmaount, double tankCapacity):this(brand,model,capacity,fuelAmaount)
+            {
+                this.Brand = brand;
+                this.Model = model;
+                this.Engine = new Engine(capacity, fuelAmaount, tankCapacity);
+            }
+            public Car (string  Brand, string Model, Engine engine)
+            {
+                this.Brand = Brand;
+                this.Model = Model;
+                this.Engine = engine;
+            }
+            public void Drive(int driveDistance)
+            {
+                Console.WriteLine("Jadę!");
+                int sleepTime = driveDistance * 100;
+                Thread.Sleep(sleepTime);
+                Engine.working(driveDistance);
+            }
+
+        }
+        class Engine
+        {
+            public double Capacity { get; set; }
+            public double FuelAmaount { get; set; }
+            public double FuelTankCapacity { get; } = 50.0;
+            public Engine(double capacity, double FuelAmaount) 
+            { 
+                this.Capacity = capacity;
+                this.FuelAmaount = FuelAmaount;
+                this.FuelTankCapacity = 40;
+            }
+            public Engine(double capacity, double FuelAmaount, double tankCapacity):this(capacity,FuelAmaount)
+            {
+                this.FuelTankCapacity = tankCapacity;
+            }
+            public void working(int DriveDstance)
+            {
+                double fuelLeft = FuelAmaount - (4 * Capacity / 100) * DriveDstance;
+                if (fuelLeft < 0)
+                    throw new Exception("Za mało paliwa!");
+                FuelAmaount = fuelLeft;
+                Console.WriteLine("Jestem!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj pojemność silnika w L: ");
+            int engineCapacity = inputLibrary.Int.restricted_int_input(0, int.MaxValue);
+            Console.Write("Podaj ilość paliwa w L: ");
+            int amaountOfFuel = inputLibrary.Int.restricted_int_input(0, int.MaxValue);
+            Console.Write("Podaj pojemność baku w L: ");
+            int tankCapacity = inputLibrary.Int.restricted_int_input(0, int.MaxValue);
+            Engine k = new Engine(engineCapacity, amaountOfFuel, tankCapacity);
+            Console.Write("Podaj odległość do przejechania: ");
+            int driveDistance = inputLibrary.Int.restricted_int_input(0, int.MaxValue);
+            
+            Console.Write("Podaj markę auta: ");
+            string brand = inputLibrary.String.string_input();
+            Console.Write("Podaj model auta: ");
+            string model = inputLibrary.String.string_input();
+            Car c = new Car(brand, model, k);
+            c.Drive(driveDistance);
+            Console.WriteLine("Pozostało paliwa " + k.FuelAmaount);
+        }
+    }
+}
---
+++ konstruktory/dlls/inputLibrary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inputLibrary
+{
+    public class Int
+    {
+        public static int int_input()
+        {
+            int var;
+            while (true)
+            {
+                try
+                {
+                    var = int.Parse(Console.ReadLine());
+                    if (var != null)
+                        break;
+                }
+                catch (Exception)
+                {
+                    Console.Write("Nieodpowiedni znak, podaj liczbę: ");
+                }
+            }
+            return var;
+        }
+        public static int restricted_int_input(int min, int max)
+        {
+            int var;
+            while (true)
+            {
+                var = inputLibrary.Int.int_input();
+                if (var <= max && var >= min)
+                    break;
+                Console.Write("Wprowadź liczbę z odpowiedniego przedziału: ");
+            }
+            return var;
+        }
+    }
+    public class String
+    {
+        public static string string_input()
+        {
+            string var;
+            while (true)
+            {
+                try
+                {
+                    var = Console.ReadLine();
+                    if (var != null)
+                        break;
+                }
+                catch (Exception)
+                {
+                    Console.Write("Nieodpowiedni znak!");
+                }
+            }
+            return var;
+        }
+    }
+}
